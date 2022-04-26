96bc1d8e6cfd42cfee5cbc86638ae9f00d4007f9
Yuut <ingride.reis.b@gmail.com>
2022-04-26 16:39:59 -0300

    Iniciando testes

---
+++ src/tests/pageLogin.01.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+describe('1- Teste o componente <Login.js />.', () => {
+  test('Os elementos devem respeitar a descrição para a tela de login.', () => {
+
+  });
+});
