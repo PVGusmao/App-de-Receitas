21ae66e31163456b56ba02987c8af536993c7c2a
Paulo Victor <pvmg1@hotmail.com>
2022-04-26 18:24:59 -0300

    Iniciado o header para as páginas do app de receitas

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useRouteMatch } from 'react-router-dom';
+import searchBtn from '../images/searchIcon.svg';
+import profileBtn from '../images/profileIcon.svg';
+import '../assets/header.css';
+
+function Header(props) {
+  const match = useRouteMatch();
+  const { title } = props;
+
+  return (
+    <header className="header">
+      <button type="button" className="button-header">
+        <img data-testid="profile-top-btn" src={ profileBtn } alt="search" />
+      </button>
+      <h1 data-testid="page-title">{ title }</h1>
+      {
+        ((!match.path.includes('/explore')
+        && !match.path.includes('/profile')
+        && !match.path.includes('/favorite-recipes')
+        && !match.path.includes('/done-recipes'))
+        || match.path.includes('nationalities')) && (
+          <button type="button" className="button-header">
+            <img data-testid="search-top-btn" src={ searchBtn } alt="search" />
+          </button>
+        )
+      }
+    </header>
+  );
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+}.isRequired;
+
+export default Header;
--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function ExploreFoods() {
   return (
-    <>
-      ExploreFoods
-    </>
+    <Header title="Explore Foods" />
   );
 }
 
