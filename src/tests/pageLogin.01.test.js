5b934c7aefc610f6aa8151cd067def7a34420801
Yuut <ingride.reis.b@gmail.com>
2022-04-27 14:36:37 -0300

    Testes com erro

--- src/tests/pageLogin.01.test.js
+++ src/tests/pageLogin.01.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
+/* import userEvent from '@testing-library/user-event'; */
 import { screen } from '@testing-library/react';
-import renderWithRouter from './RenderWithRouter';
+import renderWithRouter from './renderWhitRouter';
+import Login from '../pages/Login';
 
 describe('1- Teste o componente <Login.js />.', () => {
   test('Os elementos devem respeitar a descrição para a tela de login.', () => {
-
+    renderWithRouter(<Login />);
+    const email = screen.getByTestId('email-input');
+    const password = screen.getAllByTestId('password-input');
+    const button = screen.getAllByTestId('login-submit-btn');
+    expect(email).toBeInTheDocument();
+    expect(password).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
   });
 });
