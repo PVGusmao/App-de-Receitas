3aac4f041209823abb3878e16c9ceade3120f94c
Paulo Victor <pvmg1@hotmail.com>
2022-04-27 18:12:10 -0300

    finalizado requisito 16

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import UserContext from './UserContext';
 import { getByFirstLetter, getByIngredients, getByName } from '../services/api';
 
 function UserProvider({ children }) {
+  const history = useHistory();
   const [enableSearchBar, setEnableSearchBar] = useState(false);
+  const [data, setData] = useState([]);
   const [search, setSearch] = useState({
     value: '',
     selectedRadio: '',
@@ -18,19 +21,29 @@ function UserProvider({ children }) {
   const handleClick = () => {
     const { path, value, selectedRadio } = search;
     if (selectedRadio === 'ingredients') {
-      getByIngredients(path, value).then((element) => console.log(element));
+      getByIngredients(path, value).then((element) => setData(element));
     } else if (selectedRadio === 'name') {
-      getByName(path, value).then((element) => console.log(element));
+      getByName(path, value).then((element) => setData(element));
     } else if (value.length > 1) {
       global.alert('Your search must have only 1 (one) character');
     } else {
-      getByFirstLetter(path, value).then((element) => console.log(element));
+      getByFirstLetter(path, value).then((element) => setData(element));
     }
   };
 
   const handleSerchBar = () => {
     setEnableSearchBar(!enableSearchBar);
   };
+
+  useEffect(() => {
+    if (Object.keys(data).length) {
+      const { path } = search;
+      const list = data.drinks ?? data.meals;
+      if (list.length === 1) {
+        history.push(`/${path}/${list[0].idDrink ?? list[0].idMeal}`);
+      }
+    }
+  }, [data, history, search]);
 
   const { Provider } = UserContext;
   return (
