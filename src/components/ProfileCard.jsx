d63de8dcf26a1e7804966743e7aecb850deb2e62
Paulo Victor <pvmg1@hotmail.com>
2022-05-05 17:56:39 -0300

    profile-page faltando apenas requisito 87

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,13 +1,58 @@
 import React from 'react';
-import { getStorage } from '../services/storage';
+import { useHistory } from 'react-router-dom';
+import { getStorage, setStorage } from '../services/storage';
+import '../assets/profileCard.css';
 
 function ProfileCard() {
+  const history = useHistory();
+
+  const handlePush = ({ target }) => {
+    if (target.id === '') {
+      setStorage('user', { email: '' });
+      setStorage('mealsToken', null);
+      setStorage('cocktailsToken', null);
+      setStorage('doneRecipes', []);
+      setStorage('favoriteRecipes', []);
+      setStorage('inProgressRecipes', {});
+    }
+    history.push(`/${target.id}`);
+  };
+
   return (
-    <section>
-      <h1 data-testid="profile-email">{ getStorage('user').email }</h1>
-      <button data-testid="profile-done-btn" type="button">Done Recipes</button>
-      <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
-      <button data-testid="profile-logout-btn" type="button">Logout</button>
+    <section className="profile-card-wrapper">
+      <div className="button-wrapper">
+
+        <h1 data-testid="profile-email">{ getStorage('user').email }</h1>
+
+        <button
+          className="button"
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ handlePush }
+          id="done-recipes"
+        >
+          Done Recipes
+        </button>
+
+        <button
+          className="button"
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ handlePush }
+          id="favorite-recipes"
+        >
+          Favorite Recipes
+        </button>
+
+        <button
+          className="button"
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ handlePush }
+        >
+          Logout
+        </button>
+      </div>
     </section>
   );
 }
