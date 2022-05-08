25714f5c3681f5dd5afbbc074968d52d094d5c00
Gudine <gudine283@gmail.com>
2022-05-08 17:24:41 -0300

    Finaliza página de receitas feitas

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HorizontalCard from '../components/HorizontalCard';
 import Header from '../components/Header';
+import { getStorage } from '../services/storage';
 
 function DoneRecipes() {
+  const [filter, setFilter] = useState('');
+  const recipes = getStorage('doneRecipes');
+
+  const handleFilter = ({ target }) => setFilter(target.value);
+
   return (
-    <Header title="Done Recipes" />
+    <>
+      <Header title="Done Recipes" />
+      <main>
+        <section>
+          <button
+            type="button"
+            data-testid="filter-by-all-btn"
+            value=""
+            onClick={ handleFilter }
+          >
+            All
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-food-btn"
+            value="food"
+            onClick={ handleFilter }
+          >
+            Food
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-drink-btn"
+            value="drink"
+            onClick={ handleFilter }
+          >
+            Drinks
+          </button>
+        </section>
+        <section>
+          { recipes.filter((elem) => elem.type.startsWith(filter))
+            .map((elem, index) => (
+              <HorizontalCard
+                key={ elem.id }
+                index={ index }
+                recipe={ elem }
+              />
+            )) }
+        </section>
+      </main>
+    </>
   );
 }
 
