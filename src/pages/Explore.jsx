fb554e0b40391768f30e5a47801e25ca3b062491
Paulo Victor <pvmg1@hotmail.com>
2022-05-09 15:34:52 -0300

    Finalizado explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Explore() {
+  const history = useHistory();
+  const handleClick = ({ target }) => {
+    history.push(`/explore/${target.id}s`);
+  };
+
   return (
     <>
       <Header title="Explore" />
-      <button data-testid="explore-foods" type="button">
+      <button
+        onClick={ handleClick }
+        id="food"
+        data-testid="explore-foods"
+        type="button"
+      >
         Explore Foods
       </button>
-      <button data-testid="explore-drinks" type="button">
+      <button
+        onClick={ handleClick }
+        id="drink"
+        data-testid="explore-drinks"
+        type="button"
+      >
         Explore Drinks
       </button>
       <Footer />
